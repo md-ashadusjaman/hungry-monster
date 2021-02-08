db40612c1252c8f87ac19a7efa25628de6c928d0
Md.Ashadus Jaman <mdarishjaman@gmail.com>
2021-02-08 19:30:56 +0600

    assignment-5

--- hungry.js
+++ hungry.js
@@ -1,18 +1,15 @@
-const minimize = () => {
-    window.location.reload();
-};
-
 const itemButton = document.getElementById("button");
-itemButton.addEventListener("click", function() {
+itemButton.addEventListener("click", async() => {
     let items = document.getElementById("findItems").value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${items}`)
-        .then(res => res.json())
-        .then(data => showingItems(data))
+
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${items}`
+    let res = await fetch(url);
+    let data = await res.json();
+    showingItems(data);
 });
 
+
 const showingItems = data => {
-
-
     if (data.meals) {
         data.meals.forEach(meal => {
             const findResultDiv = document.getElementById("findResult");
@@ -26,8 +23,6 @@ const showingItems = data => {
             findResultDiv.appendChild(foodDiv);
         })
     } else {
-
-
         const error = document.getElementById("error-messege");
         const foodDiv = document.createElement('div');
         foodDiv.className = 'errorText';
@@ -39,36 +34,44 @@ const showingItems = data => {
     }
 }
 
-const foodDetails = foodName => {
+const foodDetails = async(foodName) => {
     const findResultDiv = document.getElementById("findResult");
     findResultDiv.style.display = "none";
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodName}`)
-        .then(res => res.json())
-        .then(data => itemDetails(data));
+    let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodName}`
+    let res = await fetch(url);
+    let data = await res.json();
+    itemDetails(data);
 }
-
-
 
 const itemDetails = data => {
     const food = data.meals[0];
-    console.log(food);
+
     const lastDiv = document.getElementById("show-details");
     const itemsDiv = document.createElement('div');
     itemsDiv.className = 'itemsInfo';
     const lastItem = `
     <img src = "${food.strMealThumb}">
-    <h5>${food.strMeal}</h5>
-    <h5>${food.strArea} Dish</h5>
-    <h5>Category: ${food.strCategory}</h5>
-    Ingredient:
+    <p> </p>
+    <h2>${food.strMeal}</h2>
+       <h5> Ingredient: </h5>
     <ol>
     <li>${food.strIngredient1}</li>
     <li>${food.strIngredient2}</li>
     <li>${food.strIngredient3}</li>
     <li>${food.strIngredient4}</li>
+    <li>${food.strIngredient5}</li>
+    <li>${food.strIngredient6}</li>
+    <li>${food.strIngredient7}</li>
+    <li>${food.strIngredient8}</li>
+    <li>${food.strIngredient9}</li>
+    <li>${food.strIngredient10}</li>
     </ol>
-    <button onclick="minimize()" class="btn bg-warning">Search Another Item</button>
+    <button onclick="minimize()" class="btn bg-warning bottom-btn">Search Another Item</button>
     `
     itemsDiv.innerHTML = lastItem;
     lastDiv.appendChild(itemsDiv);
 }
+
+const minimize = () => {
+    window.location.reload();
+};
